1dc77a8774efc8a741d455c8765016a3840c8415
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-30 18:01:25 +0900

    Comment: 기능 설명 추가

--- src/js/components/colaGenerator.js
+++ src/js/components/colaGenerator.js
@@ -1,42 +1,42 @@
 class ColaGenerator {
-    constructor() {
-        this.itemList = document.querySelector('.list-item');
+  constructor() {
+    this.itemList = document.querySelector('.list-item');
+  }
+
+  async setup() {
+    await this.loadData((json) => {
+      this.colaFactory(json);
+    });
+  }
+
+  async loadData(callback) {
+    const response = await fetch('src/js/item.json');
+
+    if (response.ok) {
+      // http 상태코드가 200 ~ 299일 경우
+      callback(await response.json());
+    } else {
+      alert('통신 에러!' + response.status);
     }
+  }
 
-    async setup() {
-        await this.loadData((json) => {
-            this.colaFactory(json);
-        });
-    }
-
-    async loadData(callback) {
-        const response = await fetch('src/js/item.json');
-
-        if (response.ok) {
-            // http 상태코드가 200 ~ 299일 경우
-            callback(await response.json()); // 응답 본문을 읽으면서 객체형태로 파싱합니다.
-        } else {
-            alert('통신 에러!' + response.status);
-        }
-    }
-
-    // 콜라를 생산하는 함수입니다.
-    colaFactory(data) {
-        const docFrag = document.createDocumentFragment();
-        data.forEach((el) => {
-            const item = document.createElement('li');
-            const itemTemplate = `
+  // 콜라 생산
+  colaFactory(data) {
+    const docFrag = document.createDocumentFragment();
+    data.forEach((el) => {
+      const item = document.createElement('li');
+      const itemTemplate = `
             <button type="button" class="btn-item" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
                 <img src="src/images/${el.img}" alt="" class="img-item">
                 <strong class="tit-item">${el.name}</strong>
                 <span class="txt-price">${el.cost}원</span>
             </button>
             `;
-            item.innerHTML = itemTemplate;
-            docFrag.appendChild(item);
-        });
-        this.itemList.appendChild(docFrag);
-    }
+      item.innerHTML = itemTemplate;
+      docFrag.appendChild(item);
+    });
+    this.itemList.appendChild(docFrag);
+  }
 }
 
 export default ColaGenerator;
